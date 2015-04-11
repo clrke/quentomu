539f28819a461a51964d16830999099f65bc3508
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-04-12 06:49:24 +0800

    test

--- quentomu/views.py
+++ quentomu/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.db.models import Q
 from .models import *
-from quentomu.gph_api_tests import Chikka_Api as chk
+from apis.gph_api_tests import Chikka_Api as chk
 import requests as rq
 from django.core.mail import send_mail
 import types
