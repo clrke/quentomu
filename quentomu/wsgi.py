811ad395be64b149858b86407e97d29fe21cb0b8
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-04-11 22:57:36 +0800

    Get ready for production

--- quentomu/wsgi.py
+++ quentomu/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quentomu.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+application = Cling(get_wsgi_application())
