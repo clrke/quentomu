c5c7f3f1a18a6211f5087c1055859520470971cc
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-04-12 07:44:32 +0800

    turn status code into string for logs

--- quentomu/views.py
+++ quentomu/views.py
@@ -16,7 +16,7 @@ def home(request):
 	import json
 	r = chk.sendMessage('dude','09161172935','SEND', 'this')
 	global content 
-	content = "I sent a message "+ r.text + " "+ r.status_code
+	content = "I sent a message "+ r.text + " "+ str(r.status_code)
 
 	send_mail('Subject here', content, 'pagong@quentomu.herokuapp.com',
     ['pjinxed.aranzaellej@gmail	.com'], fail_silently=False)
@@ -29,11 +29,11 @@ def Remittance(request):
 def DelivNotif(request):
 	r = chk.chkDeliveryOf()
 	global content
-	content = "Delivery notification: "+r.text+" "+r.status_code
+	content = "Delivery notification: "+r.text+" "+ str(r.status_code)
 def ReceiveMsgs(request):
 	r = chk.rcvMessage()
 	global content
-	content = "Message Recieved "+r.text+" "+r.status_code
+	content = "Message Recieved "+r.text+" "+ str(r.status_code)
 
 def conversation(request):
 	if request.method == 'POST':
