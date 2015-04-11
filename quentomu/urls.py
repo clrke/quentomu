c3aa3ed6620635d0c59f136bfff7c515b06dfddb
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-04-11 15:29:01 +0800

    Make website prototype

--- quentomu/urls.py
+++ quentomu/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'quentomu.views.home', name='home'),
+    url(r'^$', 'quentomu.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
