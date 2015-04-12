aec68e19329f7b00cccf486be0d59e95fb05534f
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-04-12 13:37:42 +0800

    Make heremaps work

--- apis/gph_api_tests/Here.py
+++ apis/gph_api_tests/Here.py
@@ -1,7 +1,8 @@
 import requests as rq
+from urllib.parse import urlencode
 
 app_id = "02FLzuJ5jxn5wP0aqzhD"
-app_code = "AkwssyI7EukQ3epwH8Z5-Q" 
+app_code = "AkwssyI7EukQ3epwH8Z5-Q"
 
 def geocode_search(search):
 	payload = {
@@ -10,8 +11,18 @@ def geocode_search(search):
 		'app_code' : app_code,
 		'gen': '6'
 	}
-	r = rq.get("https://geocoder.cit.api.here.com/6.2/geocode.json?",data =payload)
-	return r.text
+	r = rq.get("https://geocoder.cit.api.here.com/6.2/geocode.json?%s"%
+		urlencode(payload))
+
+	geocode = r.json()
+
+	# return geocode['Response']['View'][0]['']
+	nav_pos = geocode['Response']['View'][0]['Result'][0]['Location']['NavigationPosition'][0]
+
+	latitude = nav_pos['Latitude']
+	longitude = nav_pos['Longitude']
+
+	return "%s,%s"%(latitude, longitude)
 
 def geo_pedroute(origin, dest):
 	payload = {
@@ -21,9 +32,8 @@ def geo_pedroute(origin, dest):
 		'app_id' : app_id,
 		'mode': 'fastest;pedestrian'
 	}
-	r = rq.get("https://geocoder.cit.api.here.com/6.2/calculateroute.json?",data =payload)
+	r = rq.get("https://route.cit.api.here.com/routing/7.2/calculateroute.json",params=payload)
 	return r.text
-
 
 def geo_carroute(origin, dest):
 	payload = {
@@ -34,9 +44,15 @@ def geo_carroute(origin, dest):
 		'mode': 'fastest;car;traffic:enabled',
 		'departure': 'now'
 	}
-	r = rq.get("https://geocoder.cit.api.here.com/6.2/calculateroute.json?",data =payload)
-	return r.text
 
+	r = rq.get("https://route.cit.api.here.com/routing/7.2/calculateroute.json?%s"%
+		urlencode(payload))
+
+	route = r.json()
+
+	distance = route['response']['route'][0]['summary']['distance']
+
+	return distance
 
 def geo_transroute(origin, dest):
 	payload = {
@@ -47,5 +63,5 @@ def geo_transroute(origin, dest):
 		'mode': 'fastest;publicTransport',
 		'combineChange': 'true'
 	}
-	r = rq.get("https://geocoder.cit.api.here.com/6.2/calculateroute.json?",data =payload)
+	r = rq.get("https://route.cit.api.here.com/routing/7.2/calculateroute.json",data =payload)
 	return r.text
