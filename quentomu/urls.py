8a5b6ccff075e54a05980ac0575a85f7033cec3b
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-04-12 12:20:14 +0800

    fix for not defined string

--- quentomu/urls.py
+++ quentomu/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     #pols
-    url(r'^DN$','quentomu.views.DN',name = notification),
+    url(r'^DN$','quentomu.views.DN',name = 'notification'),
     url(r'^testDN/(?P<number>\d+)/(?P<msg>.*)$', 'quentomu.views.DeliveryNotif',name = 'DelivNotif'),
     url(r'^testDN/(?P<number>\d+)/(?P<reqID>\d+)/(?P<msg>.*)$)','quentomu.views.DeliveryNotif_reply',name = 'Reply'),
     url(r'^testInbox/$', 'quentomu.views.ReceivedMsgs',name = 'ReceiveMsgs'),
